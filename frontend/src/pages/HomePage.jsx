e9a0aa21d3c6ff5571c17c7c4db75f28c3cd7a1b
Vinay Shirole <54908194+VNY04@users.noreply.github.com>
2021-02-24 11:13:17 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Jumbotron, Container } from 'reactstrap';
+import {Button} from 'reactstrap'
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Homepage</h1>
+            <Jumbotron fluid>
+                <Container fluid>
+                    <h1 className="display-12">For Students</h1>
+                    <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, nesciunt provident. Odio, earum ut alias perspiciatis in, vel doloribus perferendis nobis quod dolore, nemo aliquam aperiam necessitatibus similique delectus voluptatum.</p>
+                    <Button color="primary">Take Loans</Button>
+                </Container>
+            </Jumbotron>
+            <Jumbotron fluid>
+                <Container fluid>
+                    <h1 className="display-12">For Lenders</h1>
+                    <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, nesciunt provident. Odio, earum ut alias perspiciatis in, vel doloribus perferendis nobis quod dolore, nemo aliquam aperiam necessitatibus similique delectus voluptatum.</p>
+                    <Button outline color="primary">Grant Loans</Button>
+                </Container>
+            </Jumbotron>
         </div>
     )
 }
